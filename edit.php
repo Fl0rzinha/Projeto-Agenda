d436642c75de58077b10ae36a28cb715fdc51282
Fl0rzinha <gabriel.florindo.souza46@gmail.com>
2023-10-26 16:22:37 -0300

    concertando erros no edit.php

--- edit.php
+++ edit.php
@@ -4,15 +4,15 @@
 
 
     // header
-    include_once("templates/header.html");
+    include_once("templates/header.php");
 ?>
     <div class="container">
         
         <!-- botao voltar -->
-        <?php include_once(templates/backbtn.html); ?>
+        <?php include_once("templates/backbtn.html"); ?>
 
         <h1 id="main-title">Editar contato</h1>
-        <form id="create-form" action="<?= $BASE_URL ?>configprocess.php" method="POST">
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
             <input type="hidden" name="type" value="edit">
 
             <!-- ID do contato a ser editado -->
